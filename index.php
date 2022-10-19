066ac310fb68f70778f9676d23e9336442ee6be4
cjgomezm01 <115484266+cjgomezm01@users.noreply.github.com>
2022-10-19 20:16:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>hola mundo carlos javier</p> Esto es una modificacion'; ?>
+ <?php echo '<p>hola mundo carlos javier</p> Esto es una modificacion <p>Esta modificacion a sido en el servidor</p>'; ?>
  </body>
 </html> 
